59b8162d8c6efd0023c74f780b30c5b43fabed2e
EastWood915 <wojtas1993@gmail.com>
2021-12-03 15:15:37 +0100

    Added DS18B20 module to project

--- Code/main/main.c
+++ Code/main/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "ds18b20.h"
 
 void app_main(void)
 {
+    ds18b20_init(15);
     while(1)
     {
         printf("Hello World!\n");
