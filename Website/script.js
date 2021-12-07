c2ea8d71e2642dd7defb9d20e3475bd67dd5904e
EastWood915 <wojtas1993@gmail.com>
2021-12-07 12:03:53 +0100

    Fixed timezone

--- Website/script.js
+++ Website/script.js
@@ -3,16 +3,15 @@ function displayTemperature(data)
     console.log(data);
 
     let temperature = data.with[0].content.temperature;
-    let date = data.with[0].created;
+    let date = new Date(data.with[0].created);
 
     document.querySelector("span.spinner-border").style.visibility = "hidden";
     document.querySelector("h1.temperature").textContent = temperature + "°C";
-    document.querySelector("span.date").textContent = date.replace('T', ' ').slice(0,-5);
+    document.querySelector("span.date").textContent = date.toLocaleDateString("pl-PL", {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'});
     document.querySelector("span.date").style.visibility = "visible";
 }
 
 fetch('https://dweet.io/get/latest/dweet/for/jbdhsuk')
   .then(response => response.json())
-//  .then(data => displayTemperature(data.with[0].content.temperature));
   .then(data => displayTemperature(data));
 
