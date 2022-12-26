9f65fed10c2100fda068f2e0483d750335e0d81a
EastWood915 <wojtas1993@gmail.com>
2022-12-26 16:16:56 +0100

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -1,12 +1,17 @@
+const TEMP_OFFSET = 7;
+const TEMP_COEF = 1;
+
 function displayTemperature(data)
 {
     console.log(data);
 
     let temperature = data.with[0].content.temperature;
     let date = new Date(data.with[0].created);
+    
+    temperature = TEMP_COEF * temperature + TEMP_OFFSET;
 
     document.querySelector("span.spinner-border").style.visibility = "hidden";
-    document.querySelector("h1.temperature").textContent = temperature + "°C";
+    document.querySelector("h1.temperature").textContent = temperature.toFixed(1) + "°C";
     document.querySelector("span.date").textContent = date.toLocaleDateString("pl-PL", {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'});
     document.querySelector("span.date").style.visibility = "visible";
 }
